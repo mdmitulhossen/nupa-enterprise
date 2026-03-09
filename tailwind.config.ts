06316ab133cb64bb4aee43b9574b4ea09ee65059
mdmitulhossen <md.mitul.hossen99@gmail.com>
2026-03-09 22:34:21 +0600

    feat: dark mode issue

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,8 @@
 import type { Config } from "tailwindcss";
 
 export default {
-  darkMode: ["class"],
+  // darkMode: ['variant'],
+  // darkMode: ["class"],
   content: ["./pages/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}", "./app/**/*.{ts,tsx}", "./src/**/*.{ts,tsx}"],
   prefix: "",
   theme: {
